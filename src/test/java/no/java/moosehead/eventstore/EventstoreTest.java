58e09fd54fb46e8db065bb7a511d3f76e59a537f
Anders Karlsen <karlsenanders@gmail.com>
2014-04-22 18:47:35 +0200

    Added dummy test and adjusted pom

--- src/test/java/no/java/moosehead/eventstore/EventstoreTest.java
+++ src/test/java/no/java/moosehead/eventstore/EventstoreTest.java
@@ -1,4 +1,14 @@
 package no.java.moosehead.eventstore;
 
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
 public class EventstoreTest {
+    @Test
+    public void shouldCreateEvent() throws Exception {
+        Eventstore eventstore = new Eventstore();
+        assertThat(eventstore).isNotNull();
+
+    }
 }
