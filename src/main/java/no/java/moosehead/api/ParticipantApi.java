6f07ad907ec6df4cda6dcb7b6c87b77585922641
Anders Karlsen <karlsenanders@gmail.com>
2014-07-08 11:40:17 +0200

    Updated api with confirm email

--- src/main/java/no/java/moosehead/api/ParticipantApi.java
+++ src/main/java/no/java/moosehead/api/ParticipantApi.java
@@ -5,6 +5,7 @@ import java.util.List;
 public interface ParticipantApi {
     public List<WorkshopInfo> workshops();
     public ParticipantActionResult reservation(String workshopid, String email, String fullname);
+    public ParticipantActionResult confirmEmail(String token);
     public ParticipantActionResult cancellation(String workshopid, String email);
     public List<ParticipantReservation> myReservations(String email);
 }
