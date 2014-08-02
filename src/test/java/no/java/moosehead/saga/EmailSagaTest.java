0154c0c45483ee7c667ae7fa30832aadb8cbd599
Anders Karlsen <karlsenanders@gmail.com>
2014-08-02 14:17:01 +0200

    Not sending emails twice

--- src/test/java/no/java/moosehead/saga/EmailSagaTest.java
+++ src/test/java/no/java/moosehead/saga/EmailSagaTest.java
@@ -3,26 +3,48 @@ package no.java.moosehead.saga;
 import no.java.moosehead.controller.SystemSetup;
 import no.java.moosehead.eventstore.ReservationAddedByUser;
 import no.java.moosehead.eventstore.system.SystemBootstrapDone;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import static org.mockito.Mockito.*;
 
 public class EmailSagaTest {
-    @Test
-    public void shouldSendEmailOnConfirm() throws Exception {
-        EmailSender emailSender = mock(EmailSender.class);
+
+    private EmailSender emailSender;
+    private EmailSaga emailSaga;
+
+    @Before
+    public void setUp() throws Exception {
+        emailSender = mock(EmailSender.class);
         SystemSetup setup = mock(SystemSetup.class);
         when(setup.emailSender()).thenReturn(emailSender);
         SystemSetup.setSetup(setup);
 
-        EmailSaga emailSaga = new EmailSaga();
+        emailSaga = new EmailSaga();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        SystemSetup.setSetup(null);
+
+    }
+
+    @Test
+    public void shouldSendEmailOnConfirm() throws Exception {
         emailSaga.eventAdded(new SystemBootstrapDone(1L));
-        emailSaga.eventAdded(new ReservationAddedByUser(System.currentTimeMillis(),2L,"darth@a.com","Darth","one"));
+        emailSaga.eventAdded(new ReservationAddedByUser(System.currentTimeMillis(), 2L, "darth@a.com", "Darth", "one"));
 
         verify(emailSender).sendEmailConfirmation("darth@a.com","2");
         verifyNoMoreInteractions(emailSender);
     }
 
+    @Test
+    public void shouldNotSendIfBootstrapNotDone() throws Exception {
+        emailSaga.eventAdded(new ReservationAddedByUser(System.currentTimeMillis(), 2L, "darth@a.com", "Darth", "one"));
 
+        verifyNoMoreInteractions(emailSender);
+
+    }
 }
