8748da606ebd22e962e42ecd1963a58278d095eb
Anders Karlsen <karlsenanders@gmail.com>
2022-08-10 21:23:27 +0200

    Added status servlet. 2022 config

---
+++ src/main/java/no/java/moosehead/web/StatusServlet.java
@@ -0,0 +1,26 @@
+package no.java.moosehead.web;
+
+import org.jsonbuddy.JsonObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+@WebServlet(urlPatterns = {"/status"})
+public class StatusServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        new JsonObject()
+                .put("status", "ok")
+                .put("localtime",LocalDateTime.now().toString())
+                .put("instantnow", Instant.now().toString())
+                .put("opentime", Instant.now().toString())
+                .toJson(resp.getWriter());
+    }
+}
